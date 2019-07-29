fe700ea97f075a5727f7ee3ebcd632cbe7f13a87
liufei <feil@keyayun.com>
2019-07-29 14:05:59 +0800

    update webpack config

---
+++ src/add.js
@@ -0,0 +1,5 @@
+import _add from 'lodash/add';
+
+const add = (a, b) => _add(a, b);
+
+export default add;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDevMode = true;
+const isEnvDev = process.env.NODE_ENV === 'development';
+const isEnvProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isDevMode ? 'development' : 'production',
+  mode: isEnvProd ? 'production' : isEnvDev && 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[chunkhash:8].bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+  ],
 };
